349dd6199d81c1850bb83b407e12bb6ac7ed5126
Hyeon Jeon <jeonhyun97@gmail.com>
2020-03-27 14:15:09 +0900

    test code added / presentation for lecture1 missing session 70% completed

---
+++ missing_session/lecture1/code/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!!" << endl;
+    cout << "what's your name?? ";
+    string name;
+    cin >> name;
+    cout << "Hi " + name +". Nice to meet you!!" << endl;
+    cout << "Welcome to the world of programming!!" << endl;
+}

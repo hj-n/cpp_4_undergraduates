a795c1f74452b2cbf053f29546f3b00eb818fbba
Hyeon Jeon <jeonhyun97@gmail.com>
2020-04-11 02:25:21 +0900

    lecture0 prerequisites updated

--- cpp/lecture0/code/15_fibonacci_normal.cpp
+++ cpp/lecture0/code/15_fibonacci_normal.cpp
@@ -8,7 +8,7 @@ int main() {
      * Problem 15
      * INPUT: natural number n
      * OUTPUT: n-th fibonacci number
-     * Limitation: use array to store fibonacci numbers
+     * Limitation: use array to store fibonacci numbers, n < 1000
      * array[i - 1] should store i-th number of fibonacci number
      */
 

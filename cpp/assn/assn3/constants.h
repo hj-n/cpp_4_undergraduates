cba5b76e79cd315759f7844735f6c0e25c5e2e8d
Hyeon Jeon <jeonhyun97@gmail.com>
2020-05-09 12:15:22 +0900

    comment update

--- cpp/assn/assn3/constants.h
+++ cpp/assn/assn3/constants.h
@@ -1,6 +1,8 @@
-#define INPUT_NUM 784  // number of input neurons (28*28)
-#define HIDDEN_NUM 128                         // number of hidden neurons
-#define OUTPUT_NUM 10                          // number of output neurons
+#define INPUT_NUM 784    // number of input neurons (28*28)
+#define HIDDEN_NUM 128   // number of hidden neurons
+#define OUTPUT_NUM 10    // number of output neurons
 
+
+// modify freely!!
 #define EPOCH 40
 #define LR 0.01
